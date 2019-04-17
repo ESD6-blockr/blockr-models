8b5b4013e3678d4e929a253442062ad9e6f0e407
Dane Naebers <danenaebers@gmail.com>
2019-04-17 10:57:53 +0200

    refactor date notation and remove deprecated deploy folder (#9)

--- src/transaction.ts
+++ src/transaction.ts
@@ -5,19 +5,19 @@ import { TransactionType } from "./transactionType";
  * Transactions
  */
 export class Transaction implements IModel  {
-  public recipientKey: string;
-  public amount: number;
-  public timestamp: number;
   public type: TransactionType;
+  public recipientKey: string;
   public senderKey: string;
+  public amount: number;
+  public date: Date;
   public signature: string;
   public blockHash: string; // TODO: WHAT IS THIS
 
-  constructor(recipientKey: string, amount: number, timestamp: number,
-              type: TransactionType, senderKey: string) {
+  constructor(type: TransactionType, recipientKey: string,
+              senderKey: string, amount: number, date: Date) {
     this.recipientKey = recipientKey;
     this.amount = amount;
-    this.timestamp = timestamp;
+    this.date = date;
     this.type = type;
     this.senderKey = senderKey;
   }
