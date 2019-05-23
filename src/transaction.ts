8bd71a8db7096185d5a506a73022a8a4c95c638e
Ruud Schouten <ruudschouten@users.noreply.github.com>
2019-05-23 12:48:56 +0200

    Added Smart Contract changes. (#15)
    * Added Smart Contract changes.
    
    Added ContractId and ContractData to transaction.ts
    Added SMARTCONTRACT type to transactionType.ts
    
    * Fixed indentation
    
    * Changed requested changes
    
    Made the contractId & contractData optional.
    Changed contractId to smartContractId.
    Changed smartContractId type from Number to number.
    
    * Updated to SMART_CONTRACT
    
    * Removed checks to see if a field is undefined
    
    Changed smartContractId to string, which should later be used to store a UUID
    
    * Changed contractData to smartContractData

--- src/transaction.ts
+++ src/transaction.ts
@@ -11,13 +11,17 @@ export class Transaction implements IModel  {
   public amount: number;
   public date: Date;
   public signature: string;
+  public smartContractId?: string;
+  public smartContractData?: string;
 
-  constructor(type: TransactionType, recipientKey: string,
-              senderKey: string, amount: number, date: Date) {
+  constructor(type: TransactionType, recipientKey: string, senderKey: string, 
+              amount: number, date: Date, smartContractId?: string, smartContractData?: string) {
     this.recipientKey = recipientKey;
     this.amount = amount;
     this.date = date;
     this.type = type;
     this.senderKey = senderKey;
+    this.smartContractId = smartContractId;
+    this.smartContractData = smartContractData;
   }
 }
--- src/transactionType.ts
+++ src/transactionType.ts
@@ -1,4 +1,5 @@
 export enum TransactionType {
     COIN,
     STAKE,
+    SMART_CONTRACT,
 }
