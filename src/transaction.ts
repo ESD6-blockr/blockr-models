5e9cad18386e1e303d993b5df17bba0017b66392
Ruud Schouten <ruudschouten@users.noreply.github.com>
2019-05-23 12:25:48 +0200

    Changed contractData to smartContractData

--- src/transaction.ts
+++ src/transaction.ts
@@ -12,16 +12,16 @@ export class Transaction implements IModel  {
   public date: Date;
   public signature: string;
   public smartContractId?: string;
-  public contractData?: string;
+  public smartContractData?: string;
 
   constructor(type: TransactionType, recipientKey: string, senderKey: string, 
-              amount: number, date: Date, smartContractId?: string, contractData?: string) {
+              amount: number, date: Date, smartContractId?: string, smartContractData?: string) {
     this.recipientKey = recipientKey;
     this.amount = amount;
     this.date = date;
     this.type = type;
     this.senderKey = senderKey;
     this.smartContractId = smartContractId;
-    this.contractData = contractData;
+    this.smartContractData = smartContractData;
   }
 }
